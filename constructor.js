899edaba6abd9cb25cff618764a9d3ab4de9b459
stephanie-niva <stephanienivaa@example.com>
2024-04-26 11:54:07 +0300

    add changes

--- constructor.js
+++ constructor.js
@@ -10,7 +10,7 @@ class Car {
     }
 }
 const bmw = new Car('BMW', 'M4-Convertable', 2014, 'true')
-console.log({jeep});
+console.log({bmw});
 // Rental class
 class Rental {
     constructor(car, renterName, rentalStartDate, rentalEndDate) {
@@ -27,3 +27,49 @@ class Rental {
 }
 const myRental = new Rental ('Toyota', 'Ben', "2024-4-20", "2024-5-10")
 console.log({myRental});
+
+/*Q2*/
+class Question {
+    constructor(text, options, correctAnswer) {
+      this.text = text;
+      this.options = options;
+      this.correctAnswer = correctAnswer;
+    }
+    checkAnswer(userAnswer) {
+      return userAnswer === this.correctAnswer;
+    }
+  }
+  const sampleQuestion = new Question(
+    "how many side does a triangle have?",
+    ["one", "two", "three", "four"],
+  );
+  console.log(sampleQuestion.checkAnswer("three"));
+  console.log(sampleQuestion.checkAnswer("four"));
+  class Quiz {
+    constructor() {
+      this.questions = [];
+      this.currentQuestionIndex = 0;
+      this.score = 0;
+    }
+    addQuestion(question) {
+      this.questions.push(question);
+    }
+    nextQuestion() {
+      this.currentQuestionIndex++;
+    }
+    submitAnswer(userAnswer) {
+      const currentQuestion = this.questions[this.currentQuestionIndex];
+      if (currentQuestion.checkAnswer(userAnswer)) {
+        this.score++;
+      }
+      this.nextQuestion();
+    }
+  }
+  const quiz = new Quiz();
+  const question1 = new Question("What is the square 3?", ["12", "8", "9"], "9");
+  const question2 = new Question("What is the first season in the year", ["Summer", "winter", "spring"], "Spring");
+  quiz.addQuestion(question1);
+  quiz.addQuestion(question2);
+  quiz.submitAnswer("9");
+  quiz.submitAnswer("winter");
+  console.log("Score:", quiz.score);
